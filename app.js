02f1f7ef46dc0fd6165f2ce941c7265070de2f96
sbrycebarker <sbrycesti@gmail.com>
2019-02-09 12:47:05 -0700

    buttons

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+angular.module('myApp', ['ui.router']).config(function($urlRouterProvider, $stateProvider){
 
+// creat routes
+
+  $urlRouterProvider.when('', '/');
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'main.html',
+    controller: 'ctrl'
+  })
+  .state('view2', {
+    url: '/view2',
+    templateUrl: 'view2.html'
+  })
+
+})
--- ctrl.js
+++ ctrl.js
@@ -1,1 +1,7 @@
+//  Set up angular controller
 
+angular.module('myApp').controller('ctrl', function($scope) {
+
+  $scope.data = "Thank you for watching my tutorial"
+
+})
